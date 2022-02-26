a2397ec07d998ad39d6d2028b8e9b708d65ca3c6
Fernando Gortari <fernando.gortari@gmail.com>
2022-02-26 18:48:18 -0400

    desafio finalizado

--- operacionesEnUnaTabla.sql
+++ operacionesEnUnaTabla.sql
@@ -19,7 +19,6 @@ INSERT INTO post
     (nombre_usuario, fecha_creacion, contenido, descripcion) 
     VALUES ('Pamela', '2022-02-23', 'Nullam molestie porta mauris. Ut feugiat, dolor a vulputate consequat.', 'Nam quis pretium neque.'),
            ('Pamela', '2022-02-24', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean et.', 'Proin aliquet rutrum.'),
-           ('Pamela', '2022-02-25', 'Suspendisse potenti. Aenean ut cursus felis. Duis massa lorem, accumsan.', 'Aenean ornare ligula.'),
            ('Carlos', '2022-02-21', 'Morbi et orci lorem. Lorem ipsum dolor sit amet, consectetur.', 'Fusce justo lacus.');
 
 -- 4. Modificar la tabla post, agregando la columna título.
@@ -57,7 +56,7 @@ CREATE TABLE comentarios(
     hora_creacion TIME NOT NULL,
     contenido VARCHAR(250) NOT NULL, 
     FOREIGN KEY(id) REFERENCES
-    post(id);
+    post(id)
 );
 
 -- 2. Crear 2 comentarios para el post de "Pamela" y 4 para "Carlos".
@@ -66,10 +65,10 @@ INSERT INTO comentarios
     (id, fecha, hora_creacion, contenido) 
     VALUES ('1', '2022-02-25', '17:54:22', 'Nunc enim lectus, interdum a euismod ac, iaculis eget augue.'),
            ('1', '2022-02-25', '18:23:22', 'Mauris at dictum ipsum, a pulvinar lacus. Class aptent taciti.'),
-           ('2', '2022-02-25', '18:56:34', 'Mauris ac purus vel lacus tempor dignissim vitae ut felis.'),
-           ('2', '2022-02-25', '18:56:34', 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames.'),
-           ('2', '2022-02-25', '18:56:34', 'Mauris ut sodales urna, et fermentum massa. Sed eu nunc.'),
-           ('2', '2022-02-25', '18:56:34', 'Sed cursus ipsum eu libero vulputate consequat. Nam vulputate massa.');
+           ('6', '2022-02-25', '18:56:34', 'Mauris ac purus vel lacus tempor dignissim vitae ut felis.'),
+           ('6', '2022-02-25', '18:56:34', 'Pellentesque habitant morbi tristique senectus et netus et malesuada fames.'),
+           ('6', '2022-02-25', '18:56:34', 'Mauris ut sodales urna, et fermentum massa. Sed eu nunc.'),
+           ('6', '2022-02-25', '18:56:34', 'Sed cursus ipsum eu libero vulputate consequat. Nam vulputate massa.');
 
 -- 3. Crear un nuevo post para "Margarita".
 
@@ -81,8 +80,11 @@ INSERT INTO post
 
 INSERT INTO comentarios 
     (id, fecha, hora_creacion, contenido) 
-    VALUES ('4', '2022-02-20', '11:30:22', 'Donec egestas malesuada eros sed dictum. Duis interdum, urna suscipit.'),
-           ('4', '2022-02-21', '11:30:22', 'Proin eget est et lectus auctor pharetra. Etiam nec mattis.'),
-           ('4', '2022-02-22', '11:30:22', 'Nulla vitae luctus ante. Donec quis velit sed libero convallis.'),
-           ('4', '2022-02-25', '11:30:22', 'Curabitur blandit purus ac diam imperdiet mattis. Maecenas faucibus lacinia.'),
-           ('4', '2022-02-26', '11:30:22', 'Nunc enim lectus, interdum a euismod ac, iaculis eget augue.');
+    VALUES ('7', '2022-02-20', '11:30:22', 'Donec egestas malesuada eros sed dictum. Duis interdum, urna suscipit.'),
+           ('7', '2022-02-21', '11:30:22', 'Proin eget est et lectus auctor pharetra. Etiam nec mattis.'),
+           ('7', '2022-02-22', '11:30:22', 'Nulla vitae luctus ante. Donec quis velit sed libero convallis.'),
+           ('7', '2022-02-25', '11:30:22', 'Curabitur blandit purus ac diam imperdiet mattis. Maecenas faucibus lacinia.'),
+           ('7', '2022-02-26', '11:30:22', 'Nunc enim lectus, interdum a euismod ac, iaculis eget augue.');
+
+SELECT * FROM post;
+SELECT * FROM comentarios;
